d79c43a4a36eb23c5eb084027e898d1890b63605
jacopo-angeli <diobellissimo100@gmail.com>
2022-08-06 21:25:56 +0200

    random work done

--- searchResult.php
+++ searchResult.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 <head>
-  <link href="CSS/STYLESEARCHRESULT.css" rel="stylesheet">
+  <link href="CSS/STYLE_SEARCHRESULTS.css" rel="stylesheet">
   <link href="CSS/STYLE_COMMON.css" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Orbitron" />
   <meta charset="utf-8">
@@ -11,13 +11,17 @@
   <meta name="author" content="Antonio Oseliero, Angeli Jacopo, Destro Stefano, Angeloni Alberto"/>
 </head>
 <body>
-  <?php include 'SCRIPTS/header.php'; ?>
+  <?php
+  $GLOBALS['MorePage'] = false;
+  include 'SCRIPTS/header.php';
+  include 'SCRIPTS/SearchPageFunctions.php';
+  ?>
   <header>
     <h1 id="logo">DOOM WIKI</h1>
 
     <nav id="NavBar">
       <ul id="MenuBar">
-        <li class="MenuBarItem CurrentLocation" lang="en">HOMEPAGE</li>
+        <li class="MenuBarItem" lang="en"><a href="/">HOMEPAGE</a></li>
         <li class="MenuBarItemNestedList">
           <label id="NestedListLbl" for="NestedListBtn">
             TRAMA
@@ -36,27 +40,49 @@
       </ul>
       <div id="MenuUserWidget">
         <?php
-          if(isLogged())
-            printLoggedMenuWidget();
-          else
-            printDefaultMenuWidget();
+        if(isLogged())
+        printLoggedMenuWidget();
+        else
+        printDefaultMenuWidget();
         ?>
       </div>
     </nav>
   </header>
 
   <div class="main">
+    <form class="searchBar" action="searchResult.php" method="get">
+      <label for="SearchBar">NUOVA RICERCA</label>
+      <input id="SearchBar" type="text" name="SearchTerms" required>
+      <input type="submit" value="CERCA">
+      <input type="reset" value="PULISCI">
+    </form>
+    <a id="AnswerPagelink" href="#">Fai una domanda alla community!</a>
+    <span>
+      <p class="SearchTerms">"&nbsp; <?php echo $_GET["SearchTerms"];?> &nbsp;"</p>
+    </span>
     <p>Risultati della ricerca:</p>
-    <div class="SearchDeatils">
-      <p>Testo della ricerca</p>
-    </div>
+    <span>
+      <?php printSearchResult($_GET["SearchTerms"]); ?>
+    </span>
+    <?php
+    if($GLOBALS['MorePage']){ ?>
+      <a class="CurrentPage" id="FirstPage" href="questions.php?id=123&page=0">Prima Pagina</a>
+      <a class="CurrentPage" href="questions.php?id=123&page=0">Pagina Precedente</a>
+      <a class="CurrentPage" href="questions.php?id=123&page=0">1</a>
+      <a href="questions.php?id=123&page=0">2</a>
+      <a href="questions.php?id=123&page=0">3</a>
+      <span>...</span>
+      <a href="questions.php?id=123&page=0">Pagina Successiva</a>
+      <a id="LastPage" href="questions.php?id=123&page=0">Ultima Pagina</a>
+    <?php } ?>
   </div>
 
   <footer id="foot">
     <p>
       <span lang="en">&copy;Doom</span> è un marchio ragistrato <a href="https://bethesda.net/it/dashboard" target="_blank">2022 Bethesda Softworks LLC</a>,
       a ZeniMax Media company. I marchi appartengono ai rispettivi proprietari.
-      Tutti i diritti riservati.<br>
+      Tutti i diritti riservati.
+      <br>
       <br>
     </p>
     <img class="imgVadidCode" src="IMAGES/valid-xhtml10.png" alt="html valido"/>
