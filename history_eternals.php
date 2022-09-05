20a2ce7bdde72071ee9a8b6ede0ab2202d483604
FakeDaemon <oselieroantonio@gmail.com>
2022-09-05 17:10:06 +0200

    cookie

--- history_eternals.php
+++ history_eternals.php
@@ -16,21 +16,33 @@
 
 <body>
   <?php
-  require 'SCRIPTS/.php/database_connection.php';
-  include 'SCRIPTS/.php/user.php';
-
-  $user = new User($conn);
-   ?>
-   <div class="cookie-banner js-cookie-banner">
-       <p>
-           Il nostro sito utilizza dei <span lang="en">cookie</span> per personalizzare
-           il contenuto e analizzare il traffico di rete.</br>
-           <a href=cookie_informativa.php>Leggi di più riguardo ai <span lang="en">cookie</span></a></br>
-       </p>
-       <button class="js-cookie-dismiss">Accetta</button>
-   </div>
-   <script type="text/javascript" src="SCRIPTS/cookie.js"></script>
-    <script type="text/javascript" src="SCRIPTS/cookie.js"></script>
+    require 'SCRIPTS/.php/database_connection.php';
+    include 'SCRIPTS/.php/user.php';
+    $user = new User($conn);
+    if(isset($_POST['CookieAccepted']) &&
+       $_POST['CookieAccepted']=='Accetta')
+    {
+      setCookie('CookieAccepted', 'Accetta',
+      'time() + (86400 * 30)');
+      $_COOKIE['CookieAccepted'] = 'Accetta';
+      header('location : history_eternals.php');
+    }
+    if( !(isset($_COOKIE['CookieAccepted'])) ||
+        !($_COOKIE['CookieAccepted']=='Accetta')
+      )
+    {
+  ?>
+      <form class="cookie-banner" action="history_eternals.php" method="post">
+          <p>
+              Il nostro sito utilizza dei <span lang="en">cookie</span> per personalizzare
+              il contenuto e analizzare il traffico di rete.</br>
+              <a href=cookie_informativa.php>Leggi di più riguardo ai <span lang="en">cookie</span></a></br>
+          </p>
+          <input type="submit" name="CookieAccepted" value="Accetta">
+      </form>
+  <?php
+  }
+  ?>
     <header>
         <h1 id="logo">DOOM WIKI</h1>
         <nav id="NavBar">
