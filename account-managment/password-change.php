fe9ef659a87fed7a453e015287ed19f3dfb26690
jacopo-angeli <jacopo.angeli@studenti.unipd.it>
2022-09-12 18:05:34 +0200

    FIX: Angeloni valida

--- account-managment/password-change.php
+++ account-managment/password-change.php
@@ -17,7 +17,9 @@
   $level = 1;
   require '../SCRIPTS/.php/database_connection.php';
   include '../SCRIPTS/.php/user.php';
-  if (!(isset($_COOKIE['CookieAccepted'])) || !($_COOKIE['CookieAccepted'] == 'Accetta')) {header("location:../cookie_informativa.php");}
+  if (!(isset($_COOKIE['CookieAccepted'])) || !($_COOKIE['CookieAccepted'] == 'Accetta')) {
+    header("location:../cookie_informativa.php");
+  }
   $user = new User($conn);
   $GLOBALS['wrongPass'] = false;
   $GLOBALS['inputDifferent'] = false;
@@ -42,7 +44,7 @@
   }
   ?>
   <header>
-  <h1 id="logo">DOOM WIKI</h1>
+    <h1 id="logo">DOOM WIKI</h1>
     <label id="BurgherButtonLabel" for="BurgherButton">
       Menu
     </label>
@@ -67,25 +69,24 @@
         <li class="MenuBarItem"><a href="../trivia.php">CURIOSITÀ</a></li>
       </ul>
       <div id="MenuUserWidget">
-        <div id="MenuUserWidget">
-          <div>
-            <?php
-            if ($user->isLogged()) echo "<p>" . $user->user_name . "</p>";
-            else echo "<p>OSPITE</p>";
-            if ($user->isLogged()) {
-              if ($user->isSuperUser()) echo "<a href='../siteManager.php'>Gestione Sito</a>";
-              echo "<a href='../account-managment.php'>Impostazioni</a>";
-            } else {
-              echo "<a href='../signup.php'>Registrati</a>";
-              echo "<a href='../login.php'>Entra</a>";
-            }
-            ?>
-          </div>
+        <div>
           <?php
-          if ($user->isLogged()) echo "<img src='..//IMAGES/ProfilePics/ProfilePicN" . $user->profile_pic . ".jpg' alt='Doomguy, accedi o registrati!'>";
-          else echo "<img src='../IMAGES/ProfilePics/ProfilePicN1.jpg' alt='Doomguy, accedi o registrati!'>";
+          if ($user->isLogged()) echo "<p>" . $user->user_name . "</p>";
+          else echo "<p>OSPITE</p>";
+          if ($user->isLogged()) {
+            if ($user->isSuperUser()) echo "<a href='../siteManager.php'>Gestione Sito</a>";
+            echo "<a href='../account-managment.php'>Impostazioni</a>";
+          } else {
+            echo "<a href='../signup.php'>Registrati</a>";
+            echo "<a href='../login.php'>Entra</a>";
+          }
           ?>
         </div>
+        <?php
+        if ($user->isLogged()) echo "<img src='..//IMAGES/ProfilePics/ProfilePicN" . $user->profile_pic . ".jpg' alt='Doomguy, accedi o registrati!'>";
+        else echo "<img src='../IMAGES/ProfilePics/ProfilePicN1.jpg' alt='Doomguy, accedi o registrati!'>";
+        ?>
+      </div>
     </nav>
   </header>
   <div class="main">
